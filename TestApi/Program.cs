529a7bae94b68ff7b2ef44cd3732ff09abe51e17
Ludvig Hagmar <ludvig.hagmar@stratiteq.com>
2022-02-09 08:54:57 +0100

    Update

--- TestApi/Program.cs
+++ TestApi/Program.cs
@@ -1,9 +1,12 @@
+using TestApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => options.SchemaFilter<XEnumNamesSchemaFilter>()
+);
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(
